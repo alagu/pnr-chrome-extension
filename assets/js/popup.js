58992ff3e462574773e74c3972fc3faf6dd1bede
Alagu <alagu@alagu.net>
2014-06-27 23:12:41 +0530

    Show notice

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,4 +1,10 @@
 setTimeout(function() {
     PNRStatus.init();
     $("#add-pnr").focus();
+    $.get("http://pnrapi.alagu.net/status", function(response) {
+      if(response.trim().length > 0)
+      {
+        $("#notice").html(response).show();
+      }
+    })
 }, 2000);
